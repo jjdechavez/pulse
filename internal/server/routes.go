b950d85ab1fdb06f15bcb85aa406d3ca9b138783
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2024-05-05 16:28:50 +0800

    Make "/" as web route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -13,31 +13,31 @@ import (
 func (s *Server) RegisterRoutes() http.Handler {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.HelloWorldHandler)
+	// mux.HandleFunc("/", s.HelloWorldHandler)
 	mux.HandleFunc("/name", s.ParamHandler)
 
 	mux.HandleFunc("/health", s.healthHandler)
 
 	fileServer := http.FileServer(http.FS(web.Files))
 	mux.Handle("/assets/", fileServer)
-	mux.Handle("/web", templ.Handler(web.HelloForm()))
+	mux.Handle("/", templ.Handler(web.HelloForm()))
 	mux.HandleFunc("/hello", web.HelloWebHandler)
 	mux.HandleFunc("/endpoints/upload", web.EndpointUploadWebHandler)
 
 	return mux
 }
 
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
-}
+// func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+// 	resp := make(map[string]string)
+// 	resp["message"] = "Hello World"
+//
+// 	jsonResp, err := json.Marshal(resp)
+// 	if err != nil {
+// 		log.Fatalf("error handling JSON marshal. Err: %v", err)
+// 	}
+//
+// 	_, _ = w.Write(jsonResp)
+// }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 	jsonResp, err := json.Marshal(s.db.Health())
